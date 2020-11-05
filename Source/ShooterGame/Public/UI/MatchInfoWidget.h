6cdff4e6dd45d52a6af96b49dcd198e6dbe60a95
Drice <43438296+Drice27149@users.noreply.github.com>
2020-11-05 12:50:34 +0800

    create hud and widget class

---
+++ Source/ShooterGame/Public/UI/MatchInfoWidget.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MatchInfoWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERGAME_API UMatchInfoWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+    
+};
